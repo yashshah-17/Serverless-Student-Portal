8f372e41e803e02f3ee6c9b71f385b14c9b7f236
ssuri <sh385209@dal.ca>
2020-07-25 12:45:46 -0300

    lex support added

--- src/components/Chat/ChatWithLex.js
+++ src/components/Chat/ChatWithLex.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React from 'react';
+import Amplify from 'aws-amplify';
+import { ChatBot, AmplifyTheme } from 'aws-amplify-react';
+import awsconfig from '../../aws-exports';
 
-function ChatWithLex() {
+Amplify.configure(awsconfig);
+
+// Imported default theme can be customized by overloading attributes
+const myTheme = {
+  ...AmplifyTheme,
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: '#ff6600'
+  }
+};
+
+export default function() {
+
+  const handleComplete = (err, confirmation) => {
+    if (err) {
+      alert('Bot conversation failed')
+      return;
+    }
+
+    alert('Success: ' + JSON.stringify(confirmation, null, 2));
+    return 'Trip booked. Thank you! what would you like to do next?';
+  }
+
   return (
-    <React.Fragment>
+    <div className="App">
       <header className="App-header">
-        <p>This is System Chat page.</p>
+        <h1 className="App-title">Welcome to ChatBot Demo</h1>
       </header>
-    </React.Fragment>
+      <ChatBot
+        userInput="book ticket"
+        title="My Bot"
+        theme={myTheme}
+        botName="BookTrip_dev"
+        welcomeMessage="Welcome, how can I help you today?"
+        onComplete={handleComplete}
+        clearOnComplete={true}
+        textEnabled={true}
+        conversationModeOn={true}
+      />
+    </div>
   );
 }
-
-export default ChatWithLex;
