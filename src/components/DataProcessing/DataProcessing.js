2f7ddf5485d473544d6b7f7b8f83d051e0e50072
YashShah-17 <34470036+yashshah-17@users.noreply.github.com>
2020-07-25 22:09:46 -0300

    Created Data Processing module

--- src/components/DataProcessing/DataProcessing.js
+++ src/components/DataProcessing/DataProcessing.js
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
+import WordCloud from "react-d3-cloud";
+import NavHeader2 from "../Navbar/NavHeader2";
 
-function DataProcessing() {
-  return (
-    <React.Fragment>
-      <header className="App-header">
-        <p>This is Data Processing Page.</p>
-      </header>
-    </React.Fragment>
-  );
+class DataProcessing extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://helloworld-ex2gcdwlnq-uc.a.run.app/")
+      .then((response) => response.json())
+      .then((data) => this.setState({ data }));
+  }
+
+  render() {
+    const data = [
+      { text: "Microsoft", value: 16 },
+      { text: "Mr", value: 10 },
+      { text: "Chineese", value: 7 },
+      { text: "China", value: 6 },
+      { text: "Windows", value: 6 },
+      { text: "Negroponte", value: 5 },
+      { text: "Peru", value: 5 },
+      { text: "Saldarriaga", value: 5 },
+      { text: "Kyrgyz", value: 4 },
+      { text: "Huaral", value: 4 },
+      { text: "Stone", value: 4 },
+      { text: "Cepes", value: 4 },
+      { text: "Valley", value: 4 },
+      { text: "Republic", value: 4 },
+      { text: "Soviet", value: 2 },
+      { text: "Huaral", value: 2 },
+      { text: "Serbia", value: 2 },
+      { text: "News", value: 2 },
+      { text: "Net", value: 2 },
+      { text: "Stephen", value: 2 },
+      { text: "Toulouse", value: 2 },
+      { text: "Software", value: 2 },
+      { text: "Sybari", value: 2 },
+      { text: "Media", value: 2 },
+      { text: "World", value: 2 },
+      { text: "Nokia", value: 2 },
+      { text: "Technology", value: 2 },
+      { text: "Century", value: 2 },
+      { text: "Brussels", value: 2 },
+    ];
+
+    const fontSizeMapper = (word) => Math.log2(word.value) * 30;
+
+    return <WordCloud data={data} fontSizeMapper={fontSizeMapper} />;
+  }
 }
 
 export default DataProcessing;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <React.Fragment>
       <header className="App-header">
-        <p>This is Home.</p>
+        <p>This is Home page.</p>
       </header>
     </React.Fragment>
   );
