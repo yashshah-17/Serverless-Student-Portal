36da0da53652b8d4eb3c6fb0be797e54bd13617b
Hardik <hardikdudhrejia.0719@gmail.com>
2020-07-26 01:06:50 -0300

    Modified UI for machine learning module

--- src/components/MachineLearning/MachineLearning.js
+++ src/components/MachineLearning/MachineLearning.js
@@ -1,24 +1,36 @@
 import React from "react";
+import NavHeader from "../Navbar/NavHeader";
 
 function MachineLearning() {
 
 
- 
-
   return (
+    <div>
+       <NavHeader />
+    
     <div className="container">
-      <h3>Machine learning module</h3>
+      <br/>
+      <h1 align="center">Welcome to the Machine Learning Dashboard</h1>
+      <h5 align="center">Upload some files and see how clusters are formed</h5>
 
-      <form method="post" action="https://us-central1-instant-grove-279600.cloudfunctions.net/uploadFile" enctype="multipart/form-data">
-          <input type="file" id="file" name="file"></input>
-          <input type="submit" value="upload"></input>
-      </form>
+      <br/><br/><br/>
+      <center>
+        <form method="post" action="https://us-central1-instant-grove-279600.cloudfunctions.net/uploadFile" enctype="multipart/form-data">
+            <input type="file" id="file" name="file"></input>
+            <input type="submit" className="btn btn-success" value="upload"></input>
+        </form>
+      </center>
 
-      <form method="post" action="https://us-central1-instant-grove-279600.cloudfunctions.net/formClusters">
-        <input type="submit" value="Create Clusters"></input>
-      </form>
+      <br/>
+      <hr/>
+      <br/>
+      <center>
+        <form method="post" action="https://us-central1-instant-grove-279600.cloudfunctions.net/formClusters">
+          <input type="submit" className="btn btn-primary" value="Create Clusters"></input>
+        </form>
+      </center>
 
-
+      </div>
     </div>
   );
 }
